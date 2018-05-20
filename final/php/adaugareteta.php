29b39709909502f6b33fe43f8ed5dede365fd868
BiancaBulai <bianca.bulai@gmail.com>
2018-05-20 17:33:14 +0300

    Add files via upload

--- final/php/adaugareteta.php
+++ final/php/adaugareteta.php
@@ -1,3 +1,4 @@
+<?php include('server.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,78 +29,86 @@
         <div class="card a">
             <div class="card-body">
                 <h2>Indroduceti reteta aici </h2><br><br>
-    <form method="post" action="../php/adaugareteta.php">
-        <div class="form-group">
-        <label for="titlu">Nume Reteta</label>
-        <input type="text" id="titlu" name="titlu" >
-        </div>
-        <div class="form-group">
-        <label for="durata">Timp de preparare</label>
-            <h1>Durata</h1>
-            <input type="text" id="durata" name="durata" min="1" max="1440">
-            <h1>Minute</h1>
-            <select id="unitate" name="unitate">
-                <option value="minute">Minute</option>
-                <option value="ore">Ore</option>
-            </select>
-        </div>
-        <div class="form-group">
-        <label for="mese">Masa</label>
-        <select id="masa" name="masa">
-            <option value="micdejun">Mic Dejun</option>
-            <option value="pranz">Pranz</option>
-            <option value="cina">Cina</option>
-            <option value="gustare">Gustare</option>
-        </select>
-        </div>
-        <div class="form-group">
-        <label for="numeb">Bucatarie</label>
-        <input type="text" id="numeb" name="numeb" >
-        </div>
-        <div class="form-group">
-        <label for="numep">Metoda de preparare</label>
-            <select id="numep" name="numep">
-                <option value="fierbere">Fierbere</option>
-                <option value="coacere">Coacere</option>
-                <option value="prajire">Prajire</option>
-                <option value="larece">La rece</option>
-            </select>
-        </div>
-        <div class="form-group">
-        <label for="numei">Ingrediente</label>
-        <input type="text" id="numei" name="numei" >
-        <label for="numeb">Cantitate ingrediente</label><br>
-       <br> <h1>Gramaj</h1>
-        <input type="text" id="gramaj" name="gramaj" >
-        <h1>Unitate</h1>
-            <select id="unitate" name="unitate">
-                <option value="grame">Grame</option>
-                <option value="kg">Kg</option>
-            </select>
-        </div>
-        <div class="form-group">
-        <label for="numei">Instrumente</label>
-        <input type="text" id="numei" name="numei" >
-        </div>
-        <div class="form-group">
-        <label for="numeb">Boli</label>
-        <input type="text" id="numeb" name="numeb" >
-        </div>
-        <div class="form-group">
-        <label for="numebr">Regim</label>
-        <input type="text" id="numer" name="numer" >
-        </div>
-        <div class="form-group">
-        <label for="numea">Alergii</label>
-        <input type="text" id="numea" name="numea" >
-        </div>
-        <div class="form-group">
-        <label for="textpasi">Pasi</label>
+                <form method="post" action="../php/adaugareteta.php">
+                    <?php include('errors.php');?>
+                <div class="form-group">
+                    <label for="retete">Nume Reteta</label>
+                    <input type="text" id="titlu" name="titlu">
+                </div>
+                <div class="form-group">
+                    <label for="timp">Timp de preparare</label>
+                    <h1>Durata</h1>
+                    <input type="text" id="durata" name="durata" >
+                    <h1>Minute</h1>
+                    <select id="minute" name="minute">
+                    <option value="minute">Minute</option>
+                    <option value="ore">Ore</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="mese">Masa</label>
+                    <select id="numemasa" name="numemasa">
+                        <option value="micdejun">Mic Dejun</option>
+                        <option value="pranz">Pranz</option>
+                        <option value="cina">Cina</option>
+                        <option value="gustare">Gustare</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="bucatarii">Bucatarie</label>
+                    <input type="text" id="numebucatarie" name="numebucatarie" >
+                </div>
+                <div class="form-group">
+                    <label for="preparare">Metoda de preparare</label>
+                    <select id="metodapreparare" name="metodapreparare">
+                    <option value="fierbere">Fierbere</option>
+                    <option value="coacere">Coacere</option>
+                    <option value="prajire">Prajire</option>
+                    <option value="larece">La rece</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="ingrediente">Ingrediente</label>
+                    <input type="text" id="numeingredient" name="numeingredient" >
+                </div>
+                    <div class="form-group">
+                        <label for="cantitate">Cantitate ingrediente</label>
+                        <h1>Gramaj</h1>
+                        <input type="text" id="gramaj" name="gramaj" >
+                        <h1>Unitate</h1>
+                        <select id="unitate" name="unitate">
+                        <option value="grame">Grame</option>
+                        <option value="kg">Kg</option>
+                        </select>
+                    </div>
+                        <div class="form-group">
+                        <label for="instrumente">Instrumente</label>
+                        <input type="text" id="numeinstruemnt" name="numeinstrument" >
+                        </div>
+                        <div class="form-group">
+                        <label for="boli">Boli</label>
+                        <input type="text" id="numeboala" name="numeboala" >
+                        </div>
+                        <div class="form-group">
+                        <label for="regim">Regim</label>
+                        <input type="text" id="numeregim" name="numeregim" >
+                        </div>
+                        <div class="form-group">
+                        <label for="alergii">Alergii</label>
+                        <input type="text" id="numealergie" name="numealergie" >
+                        </div>
+                        <div class="form-group">
+                        <label for="pasi">Pasi</label>
 
-        <textarea id="tesxtpasi" name="textpasi"  style="height:200px"></textarea>
-        </div>
-        <button type="submit" class="btn" name="reg_reteta" value="adauga">Adauga</button>
-    </form>
+                        <textarea id="tesxtpas" name="textpas"  style="height:200px"></textarea>
+                        </div>
+                    <div class="form-group">
+                        <label for="imagini">Imagine</label>
+                        <input type="text" id="cale" name="cale" >
+                    </div>
+
+                    <button type="submit" class="btn" name="reg_reteta" value="adauga">Adauga</button>
+                </form>
             </div>
         </div>
     </div>
