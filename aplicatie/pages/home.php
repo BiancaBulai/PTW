25aa62f0de48f170d2a4b7f92153a270411e7c9b
BiancaBulai <bianca.bulai@gmail.com>
2018-06-14 08:51:00 +0300

    Add files via upload

---
+++ aplicatie/pages/home.php
@@ -0,0 +1,24 @@
+<?php
+// Setari pagina
+$currentMenu  = 'acasa';
+$pageTitle    = 'Acasa';
+
+// Header
+require 'layout/header.php';
+?>
+
+<div class="box b">
+    <div class="container">
+        <div class="item">
+            <img src="assets/images/food2.png">
+            <div class="overlay"> 
+                <div class="text">Bine ai venit!</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+// Header
+//require 'layout/footer.php';
+?>
